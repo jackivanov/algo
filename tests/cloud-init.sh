41fc512c981963a6ce7a1abfadfe67bebae0dd73
Jack Ivanov <e601809@gmail.com>
2019-03-25 13:27:42 +0100

    test

--- tests/cloud-init.sh
+++ tests/cloud-init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "#!/bin/bash
 export METHOD=local
 export ONDEMAND_CELLULAR=true
 export ONDEMAND_WIFI=true
@@ -8,5 +9,9 @@ export STORE_CAKEY=true
 export LOCAL_DNS=true
 export ENDPOINT=algo.lxc
 export USERS=user1,user2
-export EXTRA_VARS="install_headers=false tests=true apparmor_enabled=false"
-export ANSIBLE_EXTRA_ARGS="--skip-tags apparmor"
+export EXTRA_VARS='install_headers=false tests=true apparmor_enabled=false'
+export ANSIBLE_EXTRA_ARGS='--skip-tags apparmor'
+export REPO_SLUG=${TRAVIS_PULL_REQUEST_SLUG:-${TRAVIS_REPO_SLUG:-trailofbits/algo}}
+export REPO_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-${TRAVIS_BRANCH:-master}}
+
+curl -s https://raw.githubusercontent.com/${TRAVIS_PULL_REQUEST_SLUG:-${TRAVIS_REPO_SLUG}}/${TRAVIS_PULL_REQUEST_BRANCH:-${TRAVIS_BRANCH}}/install.sh | sudo -E bash -x"
